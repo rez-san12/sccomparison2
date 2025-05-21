c748053adb352c632f194df97a68e8def02fdead
rez-san12 <donald.e.gutierrez.24@gmail.com>
2025-05-21 23:09:24 +0800

    Add files via upload

--- RefinedRRM.py
+++ RefinedRRM.py
@@ -1,48 +1,51 @@
-import random
+# As part of the technical assessment, this script is to do the following:
+# 1. To navigate to Google.
+# 2. Searches for "Rocket Raccoon - Marvel".
+# 3. Click on the first result of the link.
+# 4. To verify that the screenshot of the page matches an expected reference screenshot.
+# The search engine has been changed from Google to Bing due to frequent captcha blocking of this script.
+
 from PIL import Image, ImageChops
 from playwright.sync_api import sync_playwright
 
-def random_delay(min_ms=3000, max_ms=5000):
-    return random.randint(min_ms, max_ms)
-
 with sync_playwright() as p:
     
-    # To capture the expected reference
+    # To capture the expected reference.
 
-    browser = p.chromium.launch(headless=False, slow_mo=25) # Opens browser and search for Bing
+    browser = p.chromium.launch(headless=False, slow_mo=25) # Opens browser and search for Bing.
     context = browser.new_context()
     page = browser.new_page()
     page.goto('https://bing.com')
     page.wait_for_timeout(3000)
    
     page.click("[aria-label=\"0 characters out of 2000\"]") 
-    page.keyboard.type("Rocket Raccoon - Marvel", delay=25) # To search the assigned character
+    page.keyboard.type("Rocket Raccoon - Marvel", delay=25) # To search the assigned character.
     page.keyboard.press("Enter")
     page.wait_for_selector("li.b_algo h2 a", timeout=10000) 
 
     first_result = page.locator("li.b_algo h2 a").first
-    href = first_result.get_attribute("href")  # Acquires URL of the first result to avoid opening a new tab
+    href = first_result.get_attribute("href")  # Acquires URL of the first result to avoid opening a new tab.
     
-    print(f"Navigating to: {href}") # Proceed to the acquired URL of the first result and initiate snapshot for the expected.png
+    print(f"Navigating to: {href}") # Proceed to the acquired URL of the first result and initiate snapshot for the expected.png.
     page.goto(href)
     page.wait_for_load_state("load")
     page.wait_for_timeout(3000)
     page.screenshot(path="expected.png")
 
-    # To capture the actual reference
+    # To capture the actual reference.
 
-    page.goto('https://bing.com') # Opens browser and search for Bing
+    page.goto('https://bing.com') # Opens browser and search for Bing.
     page.wait_for_timeout(3000)
     
     page.click("[aria-label=\"0 characters out of 2000\"]") 
-    page.keyboard.type("Rocket Raccoon - Marvel", delay=25) # To search the assigned character once again
+    page.keyboard.type("Rocket Raccoon - Marvel", delay=25) # To search the assigned character once again.
     page.keyboard.press("Enter")
     page.wait_for_selector("li.b_algo h2 a", timeout=10000)
 
     first_result = page.locator("li.b_algo h2 a").first
-    href = first_result.get_attribute("href") # Acquires URL of the first result to avoid opening a new tab
+    href = first_result.get_attribute("href") # Acquires URL of the first result to avoid opening a new tab.
     
-    print(f"Navigating to: {href}") # Proceed to the acquired URL of the first result and initiate snapshot for the expected.png
+    print(f"Navigating to: {href}") # Proceed to the acquired URL of the first result and initiate snapshot for the expected.png.
     page.goto(href)
     page.wait_for_load_state("load")
     page.wait_for_timeout(3000)
@@ -50,7 +53,7 @@ with sync_playwright() as p:
     context.close()
     browser.close()
 
-    # To verify that the actual matches the expected reference
+    # To verify that the actual matches the expected reference.
 
     expected = Image.open("expected.png")
     actual = Image.open("actual.png")
